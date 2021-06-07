e6c765a67a27a406ff19c75b183216da2d8476c3
Nuanfeng <505228589@qq.com>
2021-06-07 21:17:17 +0800

    refine test

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
     rootDir: "./tests",
-    testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$'
+    testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
+    coverageDirectory: './tests/report/',
 };
