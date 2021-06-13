743acda7cd1082ec7446f139957ecf07d75f4a8d
hymanbuer <39608250+hymanbuer@users.noreply.github.com>
2021-06-14 01:26:06 +0800

    refined

--- tools/tiled-map-generator/index.js
+++ tools/tiled-map-generator/index.js
@@ -48,73 +48,6 @@ Layout.addAlgorithm('pack-tileset', {
     }
 });
 
-///////////////
-
-function buildTmxContent() {
-    const map = XmlBuilder.create('map', { encoding: 'utf-8' });
-    map.att('version', '1.4')
-        .att('orientation', 'orthogonal')
-        .att('renderorder', 'right-down')
-        .att('width', '40')
-        .att('height', '40')
-        .att('tilewidth', '16')
-        .att('tileheight', '16');
-
-    map.ele('tileset')
-        .att('firstgid', '1')
-        .att('name', 'outdoor')
-        .att('tilewidth', '16')
-        .att('tileheight', '16')
-        .att('tilecount', '288')
-        .att('columns', '24')
-
-        .ele('image')
-            .att('source', 'buch-outdoor.png')
-            .att('width', '384')
-            .att('height', '192')
-        .up()
-    .up();
-
-    map.ele('tileset')
-        .att('firstgid', '1')
-        .att('source', 'desert.tsx')
-    .up();
-
-    map.ele('layer')
-        .att('id', '1')
-        .att('name', 'Ground')
-        .att('width', '40')
-        .att('height', '40')
-
-        .ele('data')
-            .att('encoding', 'base64')
-            .att('compression', 'zlib')
-            .text('eJztmNkKwjAQRaN9cAPrAq5Yq3Xf6v9/nSM2VIbQJjEZR+nDwQ')
-        .up()
-    .up();
-
-    return map.end({ pretty: true });
-}
-
-function buildTsxContent() {
-    const tileset = XmlBuilder.create('tileset', { encoding: 'utf-8' });
-    tileset.att('name', 'Desert')
-        .att('tilewidth', '32')
-        .att('tileheight', '32')
-        .att('spacing', '1')
-        .att('margin', '1')
-        .att('tilecount', '48')
-        .att('columns', '8');
-
-    tileset.ele('image')
-        .att('source', 'tmw_desert_spacing.png')
-        .att('width', '265')
-        .att('height', '199')
-        .up();
-
-    return tileset.end({ pretty: true });
-}
-
 //////////////
 
 async function readImageSize(imagePath) {
@@ -136,94 +69,6 @@ async function zlibCompressAndEncodeByBase64(buffer) {
             }
         });
     });
-}
-
-async function readTilesetInfo(relativeTilesetPath, tilesize) {
-    const tilesetPath = Path.join(rootDir, relativeTilesetPath);
-    const imageSize = await readImageSize(tilesetPath);
-    const columns = Math.floor(imageSize.width / tilesize);
-
-    return {
-        imageWidth: imageSize.width,
-        imageHeight: imageSize.height,
-        tilesetName: Path.basename(relativeTilesetPath, '.png'),
-        sourceName: Path.basename(relativeTilesetPath),
-        columns: columns,
-        tilecount: columns * Math.floor(imageSize.height / tilesize),
-    };
-}
-
-function createTmxMap(mapWidth, mapHeight) {
-    return XmlBuilder.create('map', { encoding: 'utf-8' })
-        .att('version', '1.4')
-        .att('orientation', 'orthogonal')
-        .att('renderorder', 'right-down')
-        .att('width', `${mapWidth}`)
-        .att('height', `${mapHeight}`)
-        .att('tilewidth', '16')
-        .att('tileheight', '16');
-}
-
-async function appendEmbededTileset(tmxMap, firstGid, relativeTilesetPath, tilesize) {
-    const {
-        imageWidth, imageHeight,
-        tilesetName, sourceName,
-        columns, tilecount,
-    } = await readTilesetInfo(relativeTilesetPath, tilesize);
-
-    tmxMap.ele('tileset')
-        .att('firstgid', `${firstGid}`)
-        .att('name', tilesetName)
-        .att('tilewidth', `${tilesize}`)
-        .att('tileheight', `${tilesize}`)
-        .att('tilecount', `${tilecount}`)
-        .att('columns', `${columns}`)
-
-        .ele('image')
-            .att('source', sourceName)
-            .att('width', `${imageWidth}`)
-            .att('height', `${imageHeight}`)
-        .up()
-    .up();
-
-    return firstGid + tilecount;
-}
-
-async function appendExternalTileset(tmxMap, firstGid, tilesetName) {
-    tmxMap.ele('tileset')
-        .att('firstgid', `${firstGid}`)
-        .att('source', `${tilesetName}.tsx`)
-    .up();
-}
-
-async function writeXmlFile(xml, outFilePath) {
-    const content = xml.end({ pretty: true });
-    await Fs.writeFile(outFilePath, content, { encoding: 'utf-8' });
-}
-
-async function handleFile() {
-    const testPath = Path.join(__dirname, 'data/maps/hideout/inner-test.json');
-
-    let nextFirstGid = 1;
-    const addedTilesetMap = {};
-    const mapData = await Fs.readJson(testPath);
-    const layers = mapData.layer;
-    const tmxMap = createTmxMap(mapData.mapWidth, mapData.mapHeight);
-
-    for (let i = 0; i < layers.length; i++) {
-        const layer = layers[i];
-        if (!addedTilesetMap[layer.tilesetName]) {
-            addedTilesetMap[layer.tilesetName] = nextFirstGid;
-            nextFirstGid = await appendEmbededTileset(tmxMap, nextFirstGid, layer.tilesetName, layer.tilesize);
-        }
-    }
-
-    await Promise.all(layers.map(async layer => {
-        const firstGid = addedTilesetMap[layer.tilesetName];
-        await appendLayer(tmxMap, layer.id, layer.name, layer.width, layer.height, layer.data, firstGid);
-    }));
-
-    await writeXmlFile(tmxMap, Path.join(__dirname, 'inner-test.tmx'));
 }
 
 //////////////////
@@ -307,9 +152,9 @@ async function proccessTileset(tileset) {
         oldIdToNewIdMap[oldId] = newId;
 
         const frame = {};
-        frame.name = `tile_${Utils.fixedInteger(newId, 4)}.png`;
-        frame.x = (oldId % oldColumns) * tilesize;
-        frame.y = Math.floor(oldId / oldColumns) * tilesize;
+        frame.name = `tile_${Utils.fixedInteger(newId, 4)}_${oldId}.png`;
+        frame.x = Math.round(((oldId-1) % oldColumns)) * tilesize;
+        frame.y = Math.round(Math.floor((oldId-1) / oldColumns)) * tilesize;
         frame.w = frame.h = tilesize;
         frames.push(frame);
     });
